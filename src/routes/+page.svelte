76bb9732c4af20f6cca87da1ecc6931569d1d4da
lajarre <alexandre.hajjar@gmail.com>
2022-12-21 19:03:10 +0000

    🐛 Random bug fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,8 @@
-<h1>Molten Finance</h1>
-<p>Visit <a href="https://book.buttery.money">book.buttery.money</a> to read the documentation.</p>
+<div class="flex-1 p-4">
+	<h1>Molten Finance</h1>
+	<p>
+		Visit <a href="https://book.buttery.money">book.buttery.money</a> to read the documentation.
+	</p>
 
-<a href="create">Create a funding contract</a>
+	<a href="molten/create">Create a funding contract</a>
+</div>
